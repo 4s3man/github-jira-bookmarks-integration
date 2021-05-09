c715f2af4ffb211c8c2e4f06d4d93f2395584d9f
Jakub Kułaga <jakub.kulaga@mint.net.pl>
2021-05-09 02:37:15 +0200

    Fix typo.

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,9 +1,9 @@
 import {RowRepository} from "../ui/RowRepository";
 
-const UPDATE_INTERFAL_10_MIN = 600000;
+const UPDATE_INTERVAL_10_MIN = 600000;
 
 const rowRepository = new RowRepository(() => null, () => null);
 
 setInterval(function () {
     rowRepository.updateRows();
-}, UPDATE_INTERFAL_10_MIN);
+}, UPDATE_INTERVAL_10_MIN);
